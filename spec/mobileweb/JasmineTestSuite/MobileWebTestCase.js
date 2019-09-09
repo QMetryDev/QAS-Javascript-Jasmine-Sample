5f488e8c295f69825756b3a8bebddba1e383c26f
qmetrydev <dev@qmetry.com>
2019-09-09 16:42:00 +0530

    change elements

--- spec/mobileweb/JasmineTestSuite/MobileWebTestCase.js
+++ spec/mobileweb/JasmineTestSuite/MobileWebTestCase.js
@@ -1,5 +1,5 @@
 /**
-* @author: rinkesh.jain
+* @author: ankit.detroja
 *
 */
 
@@ -9,25 +9,25 @@ var commonStepsModule = require('../../../base/commonsteps');
 let actions = new commonStepsModule.CommonSteps();
 
 
-describe("JasmineTestsuite", function() {
+describe("mobileweb", function() {
   protractor.browser.waitForAngularEnabled(false);
   protractor.browser.get(browser.baseUrl);
  
-  it("Mobile Web Test case",async function() {
+  it("mobileweb",async function() {
     
          await actions.get("https://www.gmail.com"); 
          await actions.clear("email.identifierid_1"); 
          await actions.sendKeys("demoqas2019@gmail.com" , "email.identifierid_1"); 
-         await actions.click("div.div2111_1"); 
          await actions.verifyValue("email.identifierid_1" , "demoqas2019@gmail.com"); 
-         await actions.verifyPresent("span.span1111_2"); 
-         await actions.click("span.span1111_2"); 
+         await actions.verifyPresent("div.div11111_2"); 
+         await actions.click("div.div11111_1_1"); 
+         await actions.waitForVisible("password.qas2019_1"); 
          await actions.clear("password.qas2019_1"); 
          await actions.sendKeys("QAS@2019" , "password.qas2019_1"); 
-         await actions.click("div.div111"); 
+         await actions.click("div.div111_1"); 
          await actions.verifyValue("password.qas2019_1" , "QAS@2019"); 
-         await actions.verifyPresent("span.span1111_1_1"); 
-         await actions.click("span.span1111_1_1"); 
+         await actions.verifyPresent("span.span1111_1"); 
+         await actions.click("span.span1111_1"); 
   });
   
 });
